53cd2e54ea786e8b3c1658d23e540a9d5ab09ab8
etiaaa <93312098+etiaaa@users.noreply.github.com>
2023-02-08 23:50:43 +0100

    Projet JAVA

---
+++ JAVA/B2/Réalisation/evaluation_SAKOA_Etia-Anaelle/src/Perroquet.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Perroquet {
+    private  String mots;
+    public void atterir() {
+    }
+
+    public void envole() {
+    }
+
+    public void marche() {
+    }
+
+    public void parle() {
+    }
+}

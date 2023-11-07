156b6b516812ad62f7847d61207b6da382452374
etiaaa <93312098+etiaaa@users.noreply.github.com>
2023-11-07 01:21:45 +0100

    MES PROJETS

---
+++ ADMINISTRATION_BASE_DE_DONNEES/Site/Contact.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>MENTALHEAL Contact</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+<section class="home">
+            <div class="home-content">
+        <h1>How are you today !</h1>
+            </div>
+</section>
+  <div class="wrapper">
+      <form action="">
+         <h2>CONTACT US</h2>
+         <div class="input-box">
+              <button type="submit" class="btn">Facebook</button>
+              <button type="submit" class="btn">Instagram</button>
+         </div>
+	
+      </form>
+  </div>
+  
+</body>
+
+<style>
+     @import url("https://fonts.googleleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap");
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh; 
+    background: url(jj.jpg) no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+
+.home{ 
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    padding: 0 10%;
+}
+
+.home-content{
+    background: transparent;
+    max-width: 300;  
+}
+.home-content h1{
+    font-size: 40px;
+    font-weight: 500;
+    color: white;
+}
+.wrapper {
+    width: 310px;
+    height: 310px;
+    background: transparent;
+    border: 2px solid rgba(255, 255, 255, .2);
+    backdrop-filter: blur(20px);
+    box-shadow: 0 0 20px rgba(0, 0, 0, .3);
+    color: white;
+    border-radius: 60px;
+    padding: 30px 40px;
+}
+
+.wrapper h2 {
+    font-size: 36 px;
+    text-align: center;
+    color: white;
+}
+.wrapper .input-box {
+    position: relative; 
+    width: 100%;
+    height: 50px;
+    margin: 30px 0;
+    color: white;
+}
+
+.wrapper .input-box input {
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    border: none;
+    outline: none;
+    border: 2px solid rgba(255, 255, 255, .2);
+    border-radius: 60px;
+    font-size: 16px;
+    color: white;
+    padding: 20px 40px 20px 20px;
+}
+ 
+.input-box input::placeholder {
+    color: white;
+    text-align: center;
+    font-size: 30px;
+ 
+    
+}
+
+.wrapper .btn {
+    padding: 12px 22px;
+    width: 200px;
+    background: transparent;
+    border: none;
+    outline: none;
+    border-radius: 60px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .5);
+    cursor: pointer;
+    font-size: 25px;
+    color: white;
+    margin-bottom: 30px;
+    font-weight: 300px;
+    
+}
+
+
+
+
+</style>
+</html>
